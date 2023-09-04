5d09d08627e7da93ccb9c7071ca2648052a45a03
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-09-04 16:09:44 +0000

    fnet audit api, added more fields for payment

--- api/models.py
+++ api/models.py
@@ -1,12 +1,11 @@
-from django.db import models
-from django.conf import settings
-from PIL import Image
 import random
 from datetime import datetime
 
+from PIL import Image
+from django.conf import settings
+from django.db import models
 
 User = settings.AUTH_USER_MODEL
-
 
 BANKS = (
     ("Select bank", "Select bank"),
@@ -53,17 +52,19 @@ BANKS = (
 )
 
 AMOUNT_RECEIVED_PAID_STATUS = (
-    ("Pending","Pending"),
-    ("Paid","Paid"),
+    ("Pending", "Pending"),
+    ("Paid", "Paid"),
 )
+
+
 class Company(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
     bank = models.CharField(max_length=100, choices=BANKS, default="Access Bank")
     email = models.EmailField(max_length=255, unique=True, default='')
-    name = models.CharField(max_length=255,blank=True)
-    company_whatsapp_phone = models.CharField(max_length=15, default="",blank=True)
-    phone = models.CharField(max_length=20,blank=True)
-    account_number = models.CharField(max_length=255,blank=True)
+    name = models.CharField(max_length=255, blank=True)
+    company_whatsapp_phone = models.CharField(max_length=15, default="", blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    account_number = models.CharField(max_length=255, blank=True)
     created_month = models.CharField(max_length=10, blank=True, default="")
     created_year = models.CharField(max_length=10, blank=True, default="")
     date_created = models.DateTimeField(auto_now_add=True)
@@ -81,17 +82,18 @@ class Company(models.Model):
         self.created_year = de_date.year
         super().save(*args, **kwargs)
 
+
 class AddCompanyAmountReceived(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
-    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     amount_received = models.DecimalField(max_digits=19, decimal_places=2, blank=True)
-    receipt = models.ImageField(upload_to="receipts", default="receipt.png",blank=True)
-    unique_identifier = models.CharField(max_length=255,blank=True)
-    account_number = models.CharField(max_length=255,blank=True)
+    receipt = models.ImageField(upload_to="receipts", default="receipt.png", blank=True)
+    unique_identifier = models.CharField(max_length=255, blank=True)
+    account_number = models.CharField(max_length=255, blank=True)
     amount_received_paid = models.CharField(max_length=30, choices=AMOUNT_RECEIVED_PAID_STATUS, default="Pending")
     received_month = models.CharField(max_length=10, blank=True, default="")
     received_year = models.CharField(max_length=10, blank=True, default="")
-    transaction_id = models.CharField(max_length=255,default="",blank=True)
+    transaction_id = models.CharField(max_length=255, default="", blank=True)
     date_received = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -111,7 +113,7 @@ class AddCompanyAmountReceived(models.Model):
         de_date = my_date.date()
         self.received_month = de_date.month
         self.received_year = de_date.year
-        self.unique_identifier = self.company.name[:5] + str(random.randint(1,90000))
+        self.unique_identifier = self.company.name[:5] + str(random.randint(1, 90000))
         self.account_number = self.company.account_number
         super().save(*args, **kwargs)
 
@@ -127,16 +129,78 @@ class AddCompanyAmountReceived(models.Model):
             return "https://agencybankingnetwork.com" + self.receipt.url
         return ''
 
+
 class AddCompanyAmountPayment(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
-    company_amount = models.ForeignKey(AddCompanyAmountReceived, on_delete=models.CASCADE,related_name='company_amount')
-    company = models.ForeignKey(Company,on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=19, decimal_places=2, blank=True)
+    company_amount = models.ForeignKey(AddCompanyAmountReceived, on_delete=models.CASCADE,
+                                       related_name='company_amount')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount1 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount2 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount3 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount4 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount5 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount6 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount7 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount8 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount9 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount10 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount11 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount12 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount13 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount14 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount15 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount16 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount17 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount18 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount19 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
+    amount20 = models.DecimalField(max_digits=19, decimal_places=2, blank=True, default=0.0)
     screenshot = models.ImageField(upload_to="screenshots", default="screenshot.png")
-    unique_identifier = models.CharField(max_length=255,blank=True)
+    screenshot1 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot2 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot3 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot4 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot5 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot6 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot7 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot8 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot9 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot10 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot11 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot12 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot13 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot14 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot15 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot16 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot17 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot18 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot19 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    screenshot20 = models.ImageField(upload_to="screenshots", default="screenshot.png")
+    unique_identifier = models.CharField(max_length=255, blank=True)
     payment_month = models.CharField(max_length=10, blank=True, default="")
     payment_year = models.CharField(max_length=10, blank=True, default="")
     transaction_id = models.CharField(max_length=255, default="")
+    transaction_id1 = models.CharField(max_length=255, default="")
+    transaction_id2 = models.CharField(max_length=255, default="")
+    transaction_id3 = models.CharField(max_length=255, default="")
+    transaction_id4 = models.CharField(max_length=255, default="")
+    transaction_id5 = models.CharField(max_length=255, default="")
+    transaction_id6 = models.CharField(max_length=255, default="")
+    transaction_id7 = models.CharField(max_length=255, default="")
+    transaction_id8 = models.CharField(max_length=255, default="")
+    transaction_id9 = models.CharField(max_length=255, default="")
+    transaction_id10 = models.CharField(max_length=255, default="")
+    transaction_id11 = models.CharField(max_length=255, default="")
+    transaction_id12 = models.CharField(max_length=255, default="")
+    transaction_id13 = models.CharField(max_length=255, default="")
+    transaction_id14 = models.CharField(max_length=255, default="")
+    transaction_id15 = models.CharField(max_length=255, default="")
+    transaction_id16 = models.CharField(max_length=255, default="")
+    transaction_id17 = models.CharField(max_length=255, default="")
+    transaction_id18 = models.CharField(max_length=255, default="")
+    transaction_id19 = models.CharField(max_length=255, default="")
+    transaction_id20 = models.CharField(max_length=255, default="")
     date_paid = models.DateTimeField(auto_now_add=True)
 
     def get_agent_username(self):
@@ -158,6 +222,35 @@ class AddCompanyAmountPayment(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.screenshot.path)
+
+        save_screenshot(self.screenshot1)
+        save_screenshot(self.screenshot2)
+        save_screenshot(self.screenshot3)
+        save_screenshot(self.screenshot4)
+        save_screenshot(self.screenshot5)
+        save_screenshot(self.screenshot6)
+        save_screenshot(self.screenshot7)
+        save_screenshot(self.screenshot8)
+        save_screenshot(self.screenshot9)
+        save_screenshot(self.screenshot10)
+        save_screenshot(self.screenshot11)
+        save_screenshot(self.screenshot12)
+        save_screenshot(self.screenshot13)
+        save_screenshot(self.screenshot14)
+        save_screenshot(self.screenshot15)
+        save_screenshot(self.screenshot16)
+        save_screenshot(self.screenshot17)
+        save_screenshot(self.screenshot18)
+        save_screenshot(self.screenshot19)
+        save_screenshot(self.screenshot20)
+
+
+def save_screenshot(de_image):
+    img = Image.open(de_image.path)
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(de_image.path)
 
     def get_screenshot_pic(self):
         if self.screenshot:
--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,93 @@
 from rest_framework import serializers
+
 from .models import Company, AddCompanyAmountReceived, AddCompanyAmountPayment
+
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ['id','agent','bank','name','phone','account_number','date_created','get_agent_username','created_month','created_year','company_whatsapp_phone','email']
+        fields = ['id', 'agent', 'bank', 'name', 'phone', 'account_number', 'date_created', 'get_agent_username',
+                  'created_month', 'created_year', 'company_whatsapp_phone', 'email']
         read_only_fields = ['agent']
+
 
 class AddCompanyAmountReceivedSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddCompanyAmountReceived
-        fields = ['id','agent','company','amount_received','receipt','unique_identifier','date_received','get_agent_username','get_receipt_pic','account_number','received_month','received_year','get_company_name','get_company_email','transaction_id','amount_received_paid']
+        fields = ['id', 'agent', 'company', 'amount_received', 'receipt', 'unique_identifier', 'date_received',
+                  'get_agent_username', 'get_receipt_pic', 'account_number', 'received_month', 'received_year',
+                  'get_company_name', 'get_company_email', 'transaction_id', 'amount_received_paid']
         read_only_fields = ['agent']
+
 
 class AddCompanyAmountPaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddCompanyAmountPayment
-        fields = ['id','agent','company','amount','screenshot','unique_identifier','date_paid','get_agent_username','get_screenshot_pic','get_company_amount_received','get_amount_received_receipt','get_amount_received_date','get_company_name','get_account_number','payment_month','payment_year','get_company_phone','get_company_whatsapp_phone','company_amount','transaction_id','get_transaction_id']
+        fields = ['id', 'agent', 'company', 'amount', 'screenshot', 'unique_identifier', 'date_paid',
+                  'get_agent_username', 'get_screenshot_pic', 'get_company_amount_received',
+                  'get_amount_received_receipt', 'get_amount_received_date', 'get_company_name', 'get_account_number',
+                  'payment_month', 'payment_year', 'get_company_phone', 'get_company_whatsapp_phone', 'company_amount',
+                  'transaction_id', 'get_transaction_id',
+                  'amount1',
+                  'amount2',
+                  'amount3',
+                  'amount4',
+                  'amount5',
+                  'amount6',
+                  'amount7',
+                  'amount8',
+                  'amount9',
+                  'amount10',
+                  'amount11',
+                  'amount12',
+                  'amount13',
+                  'amount14',
+                  'amount15',
+                  'amount16',
+                  'amount17',
+                  'amount18',
+                  'amount19',
+                  'amount20',
+                  'screenshot1',
+                  'screenshot2',
+                  'screenshot3',
+                  'screenshot4',
+                  'screenshot5',
+                  'screenshot6',
+                  'screenshot7',
+                  'screenshot8',
+                  'screenshot9',
+                  'screenshot10',
+                  'screenshot11',
+                  'screenshot12',
+                  'screenshot13',
+                  'screenshot14',
+                  'screenshot15',
+                  'screenshot16',
+                  'screenshot17',
+                  'screenshot18',
+                  'screenshot19',
+                  'screenshot20',
+                  'transaction_id1',
+                  'transaction_id2',
+                  'transaction_id3',
+                  'transaction_id4',
+                  'transaction_id5',
+                  'transaction_id6',
+                  'transaction_id7',
+                  'transaction_id8',
+                  'transaction_id9',
+                  'transaction_id10',
+                  'transaction_id11',
+                  'transaction_id12',
+                  'transaction_id13',
+                  'transaction_id14',
+                  'transaction_id15',
+                  'transaction_id16',
+                  'transaction_id17',
+                  'transaction_id18',
+                  'transaction_id19',
+                  'transaction_id20',
+
+                  ]
         read_only_fields = ['agent']
